3ab50c3dc17f58b0cc6cc61dcf3af39d929ecf4b
Luciano Paes <luciano.paes@gmail.com>
2025-07-01 23:25:10 -0300

    New way to find out the lenght of the string.

--- Day1/NumberOfCaracter.py
+++ Day1/NumberOfCaracter.py
@@ -1,4 +1,7 @@
 #string = input("Tell me the string: ")
 #print(len(string))
-print(len(input("Tell me the string: ")))
+#print(len(input("Tell me the string: ")))
 
+userName = input("Tell me the UserName: ")
+length = len(userName)
+print("The string has " + str(length) + " characters.")
