4b96e0b079d234763680967e01437e5e0cc0f361
Luciano Paes <luciano.paes@gmail.com>
2025-07-28 23:38:38 -0300

    Today was to study operaton and how to use it, also, using conversion, float to int, str to int or using round function.

---
+++ Day2/FixingErrorPrint.py
@@ -0,0 +1,2 @@
+print("number of letters in your name --> " + str(len(input("Enter your name: "))))
+# This code prompts the user to enter their name and then calculates the number of letters in the
